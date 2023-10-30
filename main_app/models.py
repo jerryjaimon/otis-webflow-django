efe3bad55a07d44435e9843ea45e2144a7747ccd
jerryjaimon <jerryjaimon4321@gmail.com>
2023-10-31 01:00:28 +0530

    asset page fixed

--- main_app/models.py
+++ main_app/models.py
@@ -27,3 +27,84 @@ class Application(models.Model):
 
     def __str__(self):
         return f"Scan Result - {self.file_status}"
+
+class Scan(models.Model):
+    SCAN_STATUS_CHOICES = [
+        ('IN_QUEUE', 'In Queue'),
+        ('SCAN_INITIATED', 'Scan Initiated'),
+        ('SCAN_COMPLETED', 'Scan Completed'),
+        ('FAILED', 'Failed'),
+    ]
+    
+    scan_slug = models.CharField(max_length=255, primary_key=True)
+
+    scan_status = models.CharField(max_length=20, choices=SCAN_STATUS_CHOICES)
+    scan_created = models.DateField(default=None, null=True, blank=True)
+    scan_start = models.DateField(default=None, null=True, blank=True)
+    scan_end = models.DateField(default=None, null=True, blank=True)
+
+
+    static_scan_status = models.CharField(max_length=20, choices=SCAN_STATUS_CHOICES)
+    static_scan_start = models.DateField(default=None, null=True, blank=True)
+    static_scan_end = models.DateField(default=None, null=True, blank=True)
+
+    dynamic_scan_status = models.CharField(max_length=20, choices=SCAN_STATUS_CHOICES)
+    dynamic_scan_start = models.DateField(default=None, null=True, blank=True)
+    dynamic_scan_end = models.DateField(default=None, null=True, blank=True)
+
+    api_scan_status = models.CharField(max_length=20, choices=SCAN_STATUS_CHOICES)
+    api_scan_start = models.DateField(default=None, null=True, blank=True)
+    api_scan_end = models.DateField(default=None, null=True, blank=True)
+
+    app_hash = models.CharField(max_length=255)
+    modified_app_hash = models.CharField(max_length=255)
+    score = models.IntegerField(default=None, null=True, blank=True)    
+    file_path = models.CharField(max_length=255)
+
+    class Meta:
+        db_table = 'scan'
+
+    def __str__(self):
+        return f"Scan {self.scan_id} - {self.scan_status}"
+
+
+class Report(models.Model):
+    report_id = models.CharField(max_length=255, primary_key=True)
+    scan_id = models.ForeignKey(Scan, on_delete=models.CASCADE)
+    
+    REPORT_STATUS_CHOICES = [
+        ('IN_PROCESS', 'In Process'),
+        ('GENERATED', 'Generated'),
+        ('FAILED', 'Failed'),
+    ]
+    report_status = models.CharField(max_length=20, choices=REPORT_STATUS_CHOICES)
+    
+    score = models.IntegerField()
+    high_vulnerability_count = models.IntegerField()
+    medium_vulnerability_count = models.IntegerField()
+    low_vulnerability_count = models.IntegerField()
+    info_vulnerability_count = models.IntegerField()
+    
+    test_case_executed_count = models.IntegerField()
+    test_case_dropped_count = models.IntegerField()
+    test_case_inconclusive_count = models.IntegerField()
+    
+    m1_issues_count = models.IntegerField()
+    m2_issues_count = models.IntegerField()
+    m3_issues_count = models.IntegerField()
+    m4_issues_count = models.IntegerField()
+    m5_issues_count = models.IntegerField()
+    m6_issues_count = models.IntegerField()
+    m7_issues_count = models.IntegerField()
+    m8_issues_count = models.IntegerField()
+    m9_issues_count = models.IntegerField()
+    m10_issues_count = models.IntegerField()
+    
+    report_path = models.CharField(max_length=255)
+    
+    class Meta:
+        db_table = 'report'
+
+    def __str__(self):
+        return f"Report {self.report_id} - {self.report_status}"
+    
--- main_app/views.py
+++ main_app/views.py
@@ -1,17 +1,164 @@
 from django.urls import get_resolver
 from django.shortcuts import render
-from main_app.models import Application  # Import the Application model
+from main_app.models import Application,Scan  # Import the Application model
+from datetime import date  # Import the date module
 
 
 def index_page(request):
     applications = Application.objects.all()
     return render(request, 'detail_client.html', {'applications': applications})
 
-def asset_page(request):
-    return render(request, 'detail_asset.html')
+def asset_page(request,slug):
+    try:
+        application = Application.objects.get(app_slug=slug)
+        app_hash = application.app_hash
+        scans = Scan.objects.filter(app_hash=app_hash)
+        context = {'scans': scans}
+    except Application.DoesNotExist:
+        # Handle the case where the application with the given slug does not exist
+        return None
+    return render(request, 'detail_asset.html',context)
 
 def report_page(request):
+    scan_data()
     return render(request, 'detail_reports.html')
+
+
+
+def scan_data():
+    new_scan1 = Scan(
+        scan_slug='scan1',
+        scan_status='SCAN_COMPLETED',
+        scan_created=date.today(),
+        scan_start=date.today(),
+        scan_end=date.today(),
+        static_scan_status='SCAN_COMPLETED',
+        static_scan_start=date.today(),
+        static_scan_end=date.today(),
+        dynamic_scan_status='SCAN_COMPLETED',
+        dynamic_scan_start=date.today(),
+        dynamic_scan_end=date.today(),
+        api_scan_status='SCAN_COMPLETED',
+        api_scan_start=date.today(),
+        api_scan_end=date.today(),
+        app_hash='hash1',
+        modified_app_hash='hashh1',
+        score=50,  # Replace with an actual score
+        file_path='your_file_path',
+    )
+
+    new_scan2 = Scan(
+        scan_slug='scan2',
+        scan_status='SCAN_COMPLETED',
+        scan_created=date.today(),
+        scan_start=date.today(),
+        scan_end=date.today(),
+        static_scan_status='SCAN_COMPLETED',
+        static_scan_start=date.today(),
+        static_scan_end=date.today(),
+        dynamic_scan_status='SCAN_COMPLETED',
+        dynamic_scan_start=date.today(),
+        dynamic_scan_end=date.today(),
+        api_scan_status='SCAN_COMPLETED',
+        api_scan_start=date.today(),
+        api_scan_end=date.today(),
+        app_hash='hash2',
+        modified_app_hash='hashh2',
+        score=80,  # Replace with an actual score
+        file_path='your_file_path',
+    )
+
+    new_scan3 = Scan(
+        scan_slug='scan3',
+        scan_status='SCAN_COMPLETED',
+        scan_created=date.today(),
+        scan_start=date.today(),
+        scan_end=date.today(),
+        static_scan_status='SCAN_COMPLETED',
+        static_scan_start=date.today(),
+        static_scan_end=date.today(),
+        dynamic_scan_status='SCAN_COMPLETED',
+        dynamic_scan_start=date.today(),
+        dynamic_scan_end=date.today(),
+        api_scan_status='SCAN_COMPLETED',
+        api_scan_start=date.today(),
+        api_scan_end=date.today(),
+        app_hash='hash3',
+        modified_app_hash='hashh3',
+        score=70,  # Replace with an actual score
+        file_path='your_file_path',
+    )
+
+    new_scan4 = Scan(
+        scan_slug='scan4',
+        scan_status='SCAN_COMPLETED',
+        scan_created=date.today(),
+        scan_start=date.today(),
+        scan_end=date.today(),
+        static_scan_status='SCAN_COMPLETED',
+        static_scan_start=date.today(),
+        static_scan_end=date.today(),
+        dynamic_scan_status='SCAN_COMPLETED',
+        dynamic_scan_start=date.today(),
+        dynamic_scan_end=date.today(),
+        api_scan_status='SCAN_COMPLETED',
+        api_scan_start=date.today(),
+        api_scan_end=date.today(),
+        app_hash='hash4',
+        modified_app_hash='hashh4',
+        score=60,  # Replace with an actual score
+        file_path='your_file_path',
+    )
+
+
+    new_scan5 = Scan(
+        scan_slug='scan5',
+        scan_status='SCAN_COMPLETED',
+        scan_created=date.today(),
+        scan_start=date.today(),
+        scan_end=date.today(),
+        static_scan_status='SCAN_COMPLETED',
+        static_scan_start=date.today(),
+        static_scan_end=date.today(),
+        dynamic_scan_status='SCAN_COMPLETED',
+        dynamic_scan_start=date.today(),
+        dynamic_scan_end=date.today(),
+        api_scan_status='SCAN_COMPLETED',
+        api_scan_start=date.today(),
+        api_scan_end=date.today(),
+        app_hash='hash5',
+        modified_app_hash='hashh5',
+        score=60,  # Replace with an actual score
+        file_path='your_file_path',
+    )
+
+    new_scan6 = Scan(
+        scan_slug='scan6',
+        scan_status='SCAN_COMPLETED',
+        scan_created=date.today(),
+        scan_start=date.today(),
+        scan_end=date.today(),
+        static_scan_status='SCAN_COMPLETED',
+        static_scan_start=date.today(),
+        static_scan_end=date.today(),
+        dynamic_scan_status='SCAN_COMPLETED',
+        dynamic_scan_start=date.today(),
+        dynamic_scan_end=date.today(),
+        api_scan_status='SCAN_COMPLETED',
+        api_scan_start=date.today(),
+        api_scan_end=date.today(),
+        app_hash='hash6',
+        modified_app_hash='hashh6',
+        score=70,  # Replace with an actual score
+        file_path='your_file_path',
+    )
+
+    new_scan1.save()
+    new_scan2.save()
+    new_scan3.save()
+    new_scan4.save()
+    new_scan5.save()
+    new_scan6.save()  # Save the new_scan instance to the database
 
 
 
